9d37d3941edef54944d5ea6a61e8cb60d053d5df
Titus Wormer <tituswormer@gmail.com>
2016-11-05 21:16:09 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,223 +1,170 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module hast-to-hyperscript
- * @fileoverview Transform HAST to something else through
- *   a hyperscript DSL.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var paramCase = require('kebab-case');
 var information = require('property-information');
 var spaces = require('space-separated-tokens');
 var commas = require('comma-separated-tokens');
+var nan = require('is-nan');
 var is = require('unist-util-is');
+
+/* Expose. */
+module.exports = wrapper;
+
+/* Wrapper around `toH`. */
+function wrapper(h, node, prefix) {
+  if (typeof h !== 'function') {
+    throw new Error('h is not a function');
+  }
+
+  if (!is('element', node)) {
+    throw new Error('Expected element, not `' + node + '`');
+  }
+
+  if (prefix === null || prefix === undefined) {
+    prefix = react(h) || vdom(h) ? 'h-' : false;
+  }
+
+  return toH(h, node, {
+    prefix: prefix,
+    key: 0,
+    react: react(h),
+    vdom: vdom(h),
+    hyperscript: hyperscript(h)
+  });
+}
+
+/* Transform a HAST node through a hyperscript interface
+ * to *anything*! */
+function toH(h, node, ctx) {
+  var selector = node.tagName;
+  var properties;
+  var attributes;
+  var children;
+  var property;
+  var elements;
+  var length;
+  var index;
+  var value;
+
+  properties = node.properties;
+  attributes = {};
+
+  for (property in properties) {
+    addAttribute(attributes, property, properties[property], ctx);
+  }
+
+  if (ctx.vdom) {
+    selector = selector.toUpperCase();
+  }
+
+  if (ctx.hyperscript && attributes.id) {
+    selector += '#' + attributes.id;
+    delete attributes.id;
+  }
+
+  if ((ctx.hyperscript || ctx.vdom) && attributes.className) {
+    selector += '.' + spaces.parse(attributes.className).join('.');
+    delete attributes.className;
+  }
+
+  if (ctx.prefix) {
+    ctx.key++;
+    attributes.key = ctx.prefix + ctx.key;
+  }
+
+  elements = [];
+  children = node.children || [];
+  length = children.length;
+  index = -1;
+
+  while (++index < length) {
+    value = children[index];
+
+    if (is('element', value)) {
+      elements.push(toH(h, value, ctx));
+    } else if (is('text', value)) {
+      elements.push(value.value);
+    }
+  }
+
+  /* Ensure no React warnings are triggered for
+   * void elements having children passed in. */
+  if (elements.length === 0) {
+    elements = undefined;
+  }
+
+  return h(selector, attributes, elements);
+}
+
+/* Add `name` and its `value` to `props`. */
+function addAttribute(props, name, value, ctx) {
+  var info = information(name) || {};
+
+  /* Ignore nully, `false`, `NaN`, and falsey known
+   * booleans. */
+  if (
+    value === null ||
+    value === undefined ||
+    value === false ||
+    nan(value) ||
+    (info.boolean && !value)
+  ) {
+    return;
+  }
+
+  name = info.name || paramCase(name);
+
+  if (info.boolean) {
+    /* Treat `true` and truthy known booleans. */
+    value = '';
+  } else if (typeof value === 'object' && 'length' in value) {
+    /* Accept `array`.  Most props are space-separater. */
+    value = (info.commaSeparated ? commas : spaces).stringify(value);
+  }
+
+  value = String(value || '');
+
+  if (
+    ctx.vdom &&
+    info.name !== 'class' &&
+    (info.mustUseAttribute || !info.name)
+  ) {
+    if (!props.attributes) {
+      props.attributes = {};
+    }
+
+    props.attributes[name] = value;
+
+    return;
+  }
+
+  props[info.propertyName || name] = value;
+}
+
+/* Check if `h` is `react.createElement`.  It doesn’t accept
+ * `class` as an attribute, it must be added through the
+ * `selector`. */
+function react(h) {
+  var node = h && h('div');
+  return node && node._store && (node.key === null || node.key === undefined);
+}
+
+/* Check if `h` is `hyperscript`.  It doesn’t accept
+ * `class` as an attribute, it must be added through the
+ * `selector`. */
+function hyperscript(h) {
+  return h && h.context && h.cleanup;
+}
 
 /**
  * Check if `h` is `virtual-dom/h`.  It’s the only
- * hyperscript “compatible” interface needing `attributes`.
- *
- * @param {Function} h - Hyperscript interface.
- * @return {boolean} - Whether `h` is `virtual-dom/h`.
- */
+ * hyperscript “compatible” interface needing `attributes`. */
 function vdom(h) {
-    try {
-        return h('div').type === 'VirtualNode';
-    } catch (e) { /* Empty */ }
+  try {
+    return h('div').type === 'VirtualNode';
+  } catch (err) { /* Empty */ }
 
-    /* istanbul ignore next */
-    return false;
+  /* istanbul ignore next */
+  return false;
 }
-
-/**
- * Check if `h` is `hyperscript`.  It doesn’t accept
- * `class` as an attribute, it must be added through the
- * `selector`.
- *
- * @param {Function} h - Hyperscript interface.
- * @return {boolean} - Whether `h` is `hyperscript`.
- */
-function hyperscript(h) {
-    return h && h.context && h.cleanup;
-}
-
-/**
- * Check if `h` is `react.createElement`.  It doesn’t accept
- * `class` as an attribute, it must be added through the
- * `selector`.
- *
- * @param {Function} h - Hyperscript interface.
- * @return {boolean} - Whether `h` is `hyperscript`.
- */
-function react(h) {
-    var node = h && h('div');
-
-    return node && node._store && node.key == null;
-}
-
-/**
- * Add `name` and its `value` to `props`.
- *
- * @param {Object} props - Attributes.
- * @param {string} name - Property name.
- * @param {*} value - Property value.
- * @param {Object} ctx - Hypertext info.
- */
-function addAttribute(props, name, value, ctx) {
-    var info = information(name) || {};
-
-    /* Ignore nully, `false`, `NaN`, and falsey known
-     * booleans. */
-    if (
-        value == null ||
-        value === false ||
-        value !== value ||
-        (info.boolean && !value)
-    ) {
-        return;
-    }
-
-    name = info.name || paramCase(name);
-
-    if (info.boolean) {
-        /* Treat `true` and truthy known booleans. */
-        value = '';
-    } else if (typeof value === 'object' && 'length' in value) {
-        /* Accept `array`.  Most props are space-separater. */
-        value = (info.commaSeparated ? commas : spaces).stringify(value);
-    }
-
-    value = String(value || '');
-
-    if (
-        ctx.vdom &&
-        info.name !== 'class' &&
-        (info.mustUseAttribute || !info.name)
-    ) {
-        if (!props.attributes) {
-            props.attributes = {};
-        }
-
-        props.attributes[name] = value;
-
-        return;
-    }
-
-    props[info.propertyName || name] = value;
-}
-
-/**
- * Transform a HAST node through a hyperscript interface
- * to *anything*!
- *
- * @param {Function} h - Hyperscript interface.
- * @param {HASTElementNode} node - Element node.
- * @param {Object} ctx - Hypertext info.
- * @throws {Error} - If `node` is node an element.
- * @return {*} - Result of `h`.
- */
-function toH(h, node, ctx) {
-    var selector = node.tagName;
-    var properties;
-    var attributes;
-    var children;
-    var property;
-    var elements;
-    var length;
-    var index;
-    var value;
-
-    properties = node.properties;
-    attributes = {};
-
-    for (property in properties) {
-        addAttribute(attributes, property, properties[property], ctx);
-    }
-
-    if (ctx.vdom) {
-        selector = selector.toUpperCase();
-    }
-
-    if (ctx.hyperscript && attributes.id) {
-        selector += '#' + attributes.id;
-        delete attributes.id;
-    }
-
-    if ((ctx.hyperscript || ctx.vdom) && attributes.className) {
-        selector += '.' + spaces.parse(attributes.className).join('.');
-        delete attributes.className;
-    }
-
-    if (ctx.prefix) {
-        ctx.key++;
-        attributes.key = ctx.prefix + ctx.key;
-    }
-
-    elements = [];
-    children = node.children || [];
-    length = children.length;
-    index = -1;
-
-    while (++index < length) {
-        value = children[index];
-
-        if (is('element', value)) {
-            elements.push(toH(h, value, ctx));
-        } else if (is('text', value)) {
-            elements.push(value.value);
-        }
-    }
-
-    /* Ensure no React warnings are triggered for
-     * void elements having children passed in. */
-    if (!elements.length) {
-        elements = undefined;
-    }
-
-    return h(selector, attributes, elements);
-}
-
-/**
- * Wrapper around `toH`.
- *
- * @param {Function} h - Hyperscript interface.
- * @param {HASTElementNode} node - Element node.
- * @param {string?} [prefix] - Unique identifier.
- * @throws {Error} - If `node` is node an element.
- * @return {*} - Result of `h`.
- */
-function wrapper(h, node, prefix) {
-    if (typeof h !== 'function') {
-        throw new Error('h is not a function');
-    }
-
-    if (!is('element', node)) {
-        throw new Error('Expected element, not `' + node + '`');
-    }
-
-    if (prefix == null) {
-        prefix = react(h) || vdom(h) ? 'h-' : false;
-    }
-
-    return toH(h, node, {
-        prefix: prefix,
-        key: 0,
-        react: react(h),
-        vdom: vdom(h),
-        hyperscript: hyperscript(h)
-    });
-}
-
-/*
- * Expose.
- */
-
-module.exports = wrapper;
--- test.js
+++ test.js
@@ -1,20 +1,6 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module hast-to-hyperscript
- * @fileoverview Test suite for `hast-to-hyperscript`.
- */
-
 'use strict';
 
-/* eslint-env node */
-/* jscs:disable jsDoc */
-
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var test = require('tape');
 var u = require('unist-builder');
 var h = require('hyperscript');
@@ -23,154 +9,147 @@ var r = require('react').createElement;
 var circular = require('circular.js');
 var toH = require('../hast-to-hyperscript/index.js');
 
+/* Tests. */
+test('hast-to-hyperscript', function (t) {
+  var hast;
+
+  t.equal(typeof toH, 'function', 'should expose a function');
+
+  t.test('should throw if not given h', function (st) {
+    t.throws(function () {
+      toH(null, u('element'));
+    }, /h is not a function/);
+
+    st.end();
+  });
+
+  t.test('should throw if not given a node', function (st) {
+    t.throws(function () {
+      toH(h);
+    }, /Expected node/);
+
+    t.throws(function () {
+      toH(h, 'text');
+    }, /Expected node/);
+
+    t.throws(function () {
+      toH(h, u('text', 'value'));
+    }, /Expected element/);
+
+    st.end();
+  });
+
+  hast = u('element', {
+    tagName: 'h1',
+    properties: {id: 'a', className: ['b', 'c'], hidden: true, height: 2}
+  }, [
+    u('text', 'bravo '),
+    u('comment', 'Hello!'),
+    u('element', {
+      tagName: 'strong',
+      properties: {
+        style: 'color: red',
+        // unknown booleans are ignored.
+        ignored: false,
+        // falsey known booleans are ignore.
+        disabled: 0,
+        // Unknown props are dash-cased.
+        // Unknown lists are space-separated.
+        camelCase: ['on', 'off'],
+        // Known comma-separated lists:
+        accept: ['.jpg', '.jpeg']
+      }
+    }, [u('text', 'charlie')]),
+    u('text', ' delta')
+  ]);
+
+  t.deepEqual(
+    clean(toH(h, hast)),
+    clean(h('h1#a.b.c', {hidden: '', height: '2'}, [
+      'bravo ',
+      h('strong', {
+        style: {color: 'red'},
+        'camel-case': 'on off',
+        accept: '.jpg, .jpeg'
+      }, 'charlie'),
+      ' delta'
+    ])),
+    'should support `hyperscript`'
+  );
+
+  t.deepEqual(
+    clean(toH(v, hast)),
+    clean(v('h1#a.b.c', {
+      key: 'h-1',
+      attributes: {hidden: '', height: '2'}
+    }, [
+      'bravo ',
+      v('strong', {
+        key: 'h-2',
+        style: 'color: red',
+        accept: '.jpg, .jpeg',
+        attributes: {'camel-case': 'on off'}
+      }, 'charlie'),
+      ' delta'
+    ])),
+    'should support `virtual-dom/h`'
+  );
+
+  t.deepEqual(
+    clean(toH(r, hast)),
+    clean(r(
+      'h1',
+      {
+        key: 'h-1',
+        id: 'a',
+        className: 'b c',
+        hidden: '',
+        height: '2'
+      },
+      'bravo ',
+      r('strong', {
+        key: 'h-2',
+        style: 'color: red',
+        'camel-case': 'on off',
+        accept: '.jpg, .jpeg'
+      }, ['charlie']),
+      ' delta'
+    )),
+    'should support `React.createElement`'
+  );
+
+  t.test('should support keys', function (st) {
+    st.equal(
+      toH(h, u('element', {tagName: 'div'})).key,
+      undefined,
+      'should not patch `keys` normally'
+    );
+
+    st.equal(
+      toH(h, u('element', {tagName: 'div'}), 'prefix-').key,
+      'prefix-1',
+      'should patch `keys` when given'
+    );
+
+    st.equal(
+      toH(v, u('element', {tagName: 'div'})).key,
+      'h-1',
+      'should patch `keys` on vdom'
+    );
+
+    st.equal(
+      toH(r, u('element', {tagName: 'div'})).key,
+      'h-1',
+      'should patch `keys` on react'
+    );
+
+    st.end();
+  });
+
+  t.end();
+});
+
 /** Clean. */
 function clean(node) {
-    return JSON.parse(JSON.stringify(node, circular()));
+  return JSON.parse(JSON.stringify(node, circular()));
 }
-
-/*
- * Tests.
- */
-
-test('hast-to-hyperscript', function (t) {
-    var hast;
-
-    t.equal(typeof toH, 'function', 'should expose a function');
-
-    t.test('should throw if not given h', function (st) {
-        t.throws(function () {
-            toH(null, u('element'));
-        }, /h is not a function/);
-
-        st.end();
-    });
-
-    t.test('should throw if not given a node', function (st) {
-        t.throws(function () {
-            toH(h);
-        }, /Expected node/);
-
-        t.throws(function () {
-            toH(h, 'text');
-        }, /Expected node/);
-
-        t.throws(function () {
-            toH(h, u('text', 'value'));
-        }, /Expected element/);
-
-        st.end();
-    })
-
-    hast = u('element', {
-        tagName: 'h1',
-        properties: {id: 'a', className: ['b', 'c'], hidden: true, height: 2}
-    }, [
-        u('text', 'bravo '),
-        u('comment', 'Hello!'),
-        u('element', {
-            tagName: 'strong',
-            properties: {
-                style: 'color: red',
-                // unknown booleans are ignored.
-                ignored: false,
-                // falsey known booleans are ignore.
-                disabled: 0,
-                // Unknown props are dash-cased.
-                // Unknown lists are space-separated.
-                camelCase: ['on', 'off'],
-                // Known comma-separated lists:
-                accept: ['.jpg', '.jpeg']
-            }
-        }, [u('text', 'charlie')]),
-        u('text', ' delta')
-    ]);
-
-    t.deepEqual(
-        clean(toH(h, hast)),
-        clean(h('h1#a.b.c', {hidden: '', height: '2'}, [
-            'bravo ',
-            h('strong', {
-                'style': {
-                    'color': 'red'
-                },
-                'camel-case': 'on off',
-                accept: '.jpg, .jpeg'
-            }, 'charlie'),
-            ' delta'
-        ])),
-        'should support `hyperscript`'
-    );
-
-    t.deepEqual(
-        clean(toH(v, hast)),
-        clean(v('h1#a.b.c', {
-            key: 'h-1',
-            attributes: {hidden: '', height: '2'}
-        }, [
-            'bravo ',
-            v('strong', {
-                key: 'h-2',
-                style: 'color: red',
-                accept: '.jpg, .jpeg',
-                'attributes': {
-                    'camel-case': 'on off'
-                }
-            }, 'charlie'),
-            ' delta'
-        ])),
-        'should support `virtual-dom/h`'
-    );
-
-    t.deepEqual(
-        clean(toH(r, hast)),
-        clean(r(
-            'h1',
-            {
-                key: 'h-1',
-                id: 'a',
-                className: 'b c',
-                hidden: '',
-                height: '2'
-            },
-            'bravo ',
-            r('strong', {
-                key: 'h-2',
-                style: 'color: red',
-                'camel-case': 'on off',
-                accept: '.jpg, .jpeg'
-            }, ['charlie']),
-            ' delta'
-        )),
-        'should support `React.createElement`'
-    );
-
-    t.test('should support keys', function (st) {
-        st.equal(
-            toH(h, u('element', {tagName: 'div'})).key,
-            undefined,
-            'should not patch `keys` normally'
-        );
-
-        st.equal(
-            toH(h, u('element', {tagName: 'div'}), 'prefix-').key,
-            'prefix-1',
-            'should patch `keys` when given'
-        );
-
-        st.equal(
-            toH(v, u('element', {tagName: 'div'})).key,
-            'h-1',
-            'should patch `keys` on vdom'
-        );
-
-        st.equal(
-            toH(r, u('element', {tagName: 'div'})).key,
-            'h-1',
-            'should patch `keys` on react'
-        );
-
-        st.end();
-    });
-
-    t.end();
-});
